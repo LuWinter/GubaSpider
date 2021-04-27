4fc32b0313b919acbdcf9a3107251cf21103f1c8
LuWinter <1352252075@qq.com>
2021-04-27 15:46:15 +0800

    批量生成爬虫

--- guba/guba/spiders/__init__.py
+++ guba/guba/spiders/__init__.py
@@ -2,3 +2,14 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+# file_text = """from .GubaCrawl import GubacrawlSpider\n\n
+# class GubaCrawl%sSpider(GubacrawlSpider):
+#     name = 'GubaCrawl%s'
+#     custom_settings = {'JOBDIR': 'jobdir/%s'}\n"""
+#
+# for i in range(7, 21):
+#     file = open("./GubaCrawl%s.py" % i, "a")
+#     file_content = file_text % (i, i, i)
+#     file.write(file_content)
+#     file.close()
